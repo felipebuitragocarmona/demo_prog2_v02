3fa8e7c4568e1db5c610c71934413210c432f1d8
felipebuitragocarmona <felipebuitragocarmona@gmail.com>
2024-09-09 10:59:18 -0500

    eliminación de sumas

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def suma(x,y):
-    return x+y
-
 def resta(x,y):
     return x-y
 
-respuesta=suma(5,7)
-print(respuesta)
-respuesta=respuesta(7-5)
+respuesta=resta(7-5)
 print("resultado es "+str(respuesta))
